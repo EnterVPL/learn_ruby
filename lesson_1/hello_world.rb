4fb41d4cdafeac500ea1fb231f4b96fd48827d29
EnterVPL <woramowy@gmai.com>
2021-01-02 14:38:52 +0100

    print

--- lesson_1/hello_world.rb
+++ lesson_1/hello_world.rb
@@ -3,8 +3,18 @@ puts "Hello world"
 puts [ 12, 32, "text" ]
 puts 'second text'
 puts String(12) << String(32) << "any"
+puts ""
 
 p "Howdy world"
 p 98714
 p [ 12, 32, "text" ]
 p String(12) << String(32) << "any"
+p ""
+
+print "Sup world"
+print [ 12, 32, "text" ]
+print String(12) << String(32) << "any"
+print ""
+print "\n"
+print "it's working\nit's working too\n"
+print "wfeui" << "\n" << "joi3ij" << "\n" << "kwefew"
