12b912952feb6cd992ad1f96ea116aea53b04f19
EnterVPL <woramowy@gmai.com>
2021-01-02 14:31:54 +0100

    p

--- lesson_1/hello_world.rb
+++ lesson_1/hello_world.rb
@@ -3,3 +3,8 @@ puts "Hello world"
 puts [ 12, 32, "text" ]
 puts 'second text'
 puts String(12) << String(32) << "any"
+
+p "Howdy world"
+p 98714
+p [ 12, 32, "text" ]
+p String(12) << String(32) << "any"
