a92c9f44b04de3ad536a2d9c6e0347f8a2f7fa51
EnterVPL <woramowy@gmail.com>
2021-03-07 14:33:08 +0100

    float

--- lesson_3/numbers.rb
+++ lesson_3/numbers.rb
@@ -4,3 +4,18 @@ int2 = 123
 puts int1, int2
 puts int1 / int2
 
+float1 = 4.123
+float2 = 8.8123
+
+puts float1, float2
+puts float2 / float1
+puts float1 * float2
+puts float1 - float2
+
+puts int1 - float1
+puts int2 + float2
+puts int2 * float1
+puts int1 / float2
+puts float2 / float1
+
+puts 62137.187180740127012830912830912830912830128
